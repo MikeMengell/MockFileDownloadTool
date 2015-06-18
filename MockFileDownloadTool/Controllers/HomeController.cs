80843edd6a4b5342e274954c89e9b81f1fdc36e3
MikeMengell <mikemengell@hotmail.com>
2015-06-18 14:30:02 +0100

    Initial commit

---
+++ MockFileDownloadTool/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MockFileDownloadTool.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public FileContentResult GenerateFile(string filename, string fileContent, string contentType)
+        {
+            filename = String.IsNullOrEmpty(filename) ? "foo.txt" : filename;
+            fileContent = String.IsNullOrEmpty(fileContent) ? "Hello World" : fileContent;
+            contentType = string.IsNullOrEmpty(contentType) ? "text/plain" : contentType;
+
+            string info = fileContent;
+            byte[] data = Encoding.UTF8.GetBytes(info);
+            return File(data, contentType, filename);
+        }
+    }
+}
---
+++ MockFileDownloadTool/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MockFileDownloadTool.MvcApplication" Language="C#" %>
